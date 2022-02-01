8136208cb366a0b5c7ac54772d9dafccb0ee3b79
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2022-02-01 14:51:53 +0200

    container added

--- src/APP/App.js
+++ src/APP/App.js
@@ -1,10 +1,12 @@
 // import './App.css'
 import s from './App.module.scss'
 import FilteredArrayForm from './../FilteredArrayForm'
+import Navbar from '../components/Navbar'
 
 function App() {
   return (
     <div className={s.App}>
+      <Navbar />
       <FilteredArrayForm title="FilteredArrayForm" />
     </div>
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import Container from '../Container/Container'
+
+const Navbar = () => {
+  return (
+    <>
+      <Container>
+        {' '}
+        <h2>This is navbar</h2>
+        <hr />
+      </Container>
+    </>
+  )
+}
+
+export default Navbar
